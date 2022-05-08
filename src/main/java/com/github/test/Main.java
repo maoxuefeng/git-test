751e2581da0189ea1d306d9bd4e1043411ff5dd8
maoxuefeng <yours_mxf@163.com>
2022-05-09 00:15:56 +0800

    1st commit

---
+++ src/main/java/com/github/test/Main.java
@@ -0,0 +1,12 @@
+package com.github.test;
+
+/**
+ * @author maoxuefeng
+ * @create 2022-05-09 上午 12:02
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
